926796d90c36b7d02a5dc9d58e8cd3bdd5f1ad2c
juanm <juan@taskflow.co.za>
2017-06-26 10:10:16 +0200

    Edit uninstall script to remove make_sig symlink

--- uninstall.php
+++ uninstall.php
@@ -4,4 +4,5 @@ if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 sql('DROP TABLE IF EXISTS restapi_general, restapi_log_event_details, restapi_log_events, restapi_token_user_mapping, restapi_token_details, restapi_tokens');
 
 unlink($amp_conf['AMPWEBROOT'] . '/restapi/rest.php');
+unlink($amp_conf['AMPWEBROOT'] . '/restapi/make_sig.php');
 ?>
